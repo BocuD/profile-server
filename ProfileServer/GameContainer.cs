b88d7ae4dc4e8c60cba30f9d2cac784e82b60b7d
BocuD <duco.born@gmail.com>
2025-03-31 17:29:55 +0200

    Update GameContainer.cs

--- ProfileServer/GameContainer.cs
+++ ProfileServer/GameContainer.cs
@@ -101,8 +101,8 @@ public class GameContainer(string workingDirectory, string executable, string ar
                     {
                         //commit the new file to git
                         string gitPath = Path.Combine(Environment.CurrentDirectory, "output");
-                        string gitExe = "C:\\Program Files\\Git\\bin\\git.exe";
-                        string gitCommand = $"{gitExe} add {output} && {gitExe} commit -m \"Trace data\" && {gitExe} push";
+                        string gitExe = "\"C:\\Program Files\\Git\\bin\\git.exe\"";
+                        string gitCommand = $"& {gitExe} add {output} && {gitExe} commit -m \"Trace data\" && {gitExe} push";
 
                         using (PowerShell powerShell = PowerShell.Create())
                         {
