58e1a3a82a760caf923c414493bad409445b1132
BocuD <duco.born@gmail.com>
2025-06-03 15:42:20 +0200

    Update GameContainer.cs

--- ProfileServer/GameContainer.cs
+++ ProfileServer/GameContainer.cs
@@ -270,7 +270,7 @@ public class GameContainer(string workingDirectory, string executable, string ar
                     ProcessStartInfo startInfo = new()
                     {
                         FileName = csvToSvgPath,
-                        Arguments = $"-csvs {csvFilePath} -o {svgFile} -stats * -ignoreStats Time (sec) DynRes Percentile",
+                        Arguments = $"-csvs {csvFilePath} -o {svgFile} -stats * -ignoreStats Time DynRes Percentile",
                         RedirectStandardOutput = true,
                         RedirectStandardError = true,
                         UseShellExecute = false,
